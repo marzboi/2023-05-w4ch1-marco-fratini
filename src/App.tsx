36702da38183f17d03dbf92872d1b902537a6b2e
Marco Fratini <marcofratini@outlook.com>
2023-05-22 19:00:13 +0200

    Add gentleman information, working on making a map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
 import { Header } from "./component/header";
-import { ButtonSelectAll } from "./button";
+import { ButtonSelectAll } from "./component/button";
+import { Main } from "./component/main";
 
 function App() {
   return (
     <>
       <Header></Header>
       <ButtonSelectAll></ButtonSelectAll>
+      <Main></Main>
     </>
   );
 }
--- src/component/main.tsx
+++ src/component/main.tsx
@@ -1,7 +1,17 @@
+import { Gentleman } from "./gentleman";
+
 export function Main() {
   return (
     <main className="main">
       <ul className="gentlemen"></ul>
+      <Gentleman
+        name="Bertin Osborne"
+        status="Alive"
+        profession="Youtuber"
+        twitter="@osbourne"
+        picture="bertin.jpg"
+        alternativeText="Alex"
+      ></Gentleman>
     </main>
   );
 }
