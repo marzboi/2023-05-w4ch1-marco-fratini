0910d307d5441f5d7b782d5ab01b0a6320e3a0d4
Marco Fratini <marcofratini@outlook.com>
2023-05-22 18:10:14 +0200

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+import { Header } from "./component/header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 }
 
 export default App;
